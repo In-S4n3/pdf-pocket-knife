afec0920259cd1fc75d9e6fb5d891f78779cae5f
In-S4n3 <tiagodcpp@gmail.com>
2023-08-17 17:44:25 +0100

    Major update, made chatgpt read files w/multiple pages

--- app/api/openAi/route.ts
+++ app/api/openAi/route.ts
@@ -8,16 +8,34 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+const splitIntoChunks = (text: string, maxTokens: number) => {
+  const words = text.split(" ");
+  const chunks = [];
+  let currentChunk = "";
+
+  for (const word of words) {
+    if ((currentChunk + word).split(" ").length < maxTokens) {
+      currentChunk += " " + word;
+    } else {
+      chunks.push(currentChunk);
+      currentChunk = word;
+    }
+  }
+
+  if (currentChunk) {
+    chunks.push(currentChunk);
+  }
+
+  return chunks;
+};
+
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const body = await req.json();
     const { messages, pdfText } = body;
-
-    const instructionMessage: ChatCompletionRequestMessage = {
-      role: "system",
-      content: `You are a helpful assistant with  access to ${pdfText} designed to answer questions ONLY from the given document content else say that you don't know the answer and always answer the queries in the language they are asked in. If the 'QUESTION' is in English, answer in English. If the 'QUESTION' is in Spanish, answer in Spanish and similarly if the QUESTION' is in XYZ language, answer it in the same XYZ language. Be as accurate as possible in providing answers only from the given document context. You are not like ChatGPT that answers every question. Answer only if it found in the given document content.`,
-    };
+    const chunks = splitIntoChunks(pdfText, 2500);
+    let response: any[] = [];
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
@@ -32,15 +50,26 @@ export async function POST(req: Request) {
     if (!messages) {
       return new NextResponse("Messages are required", { status: 400 });
     }
+    for (const chunk of chunks) {
+      const instructionMessage: ChatCompletionRequestMessage = {
+        role: "system",
+        content: `You are a helpful assistant with  access to ${chunk} designed to answer questions ONLY from the given document content else say that you don't know the answer and always answer the queries in the language they are asked in. If the 'QUESTION' is in English, answer in English. If the 'QUESTION' is in Spanish, answer in Spanish and similarly if the QUESTION' is in XYZ language, answer it in the same XYZ language. Be as accurate as possible in providing answers only from the given document context. You are not like ChatGPT that answers every question. Answer only if it found in the given document content.`,
+      };
 
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [instructionMessage, ...messages],
-    });
+      const result = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [instructionMessage, ...messages],
+      });
 
-    return NextResponse.json(response.data.choices[0].message);
+      response.push(result.data.choices[0].message);
+    }
+
+    if (response.length > 1) {
+      return NextResponse.json(response[response.length - 2]);
+    }
+    return NextResponse.json(response[0]);
   } catch (error) {
-    console.log("[CODE_ERROR]", error);
+    console.dir(error, { depth: 5 });
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
