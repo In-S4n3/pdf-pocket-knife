c45239ba4261a4c2e3e71478f16a20d254b609d9
In-S4n3 <tiagodcpp@gmail.com>
2023-08-24 15:50:14 +0100

    minor fix

--- components/loader.tsx
+++ components/loader.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 export const Loader = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
+    <div className="flex flex-col items-center justify-center h-full space-y-3">
       <div className="animate-spin">
         <PocketKnife size={"70px"} />
       </div>
-      <div>Loading...</div>
+      <div className="text-lg">Loading...</div>
     </div>
   );
 };
