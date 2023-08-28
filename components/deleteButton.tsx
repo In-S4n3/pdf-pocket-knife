a0012584813e43a1174d82a8ccb576e9337fde20
In-S4n3 <tiagodcpp@gmail.com>
2023-08-28 19:20:27 +0100

    ui improvements

--- components/deleteButton.tsx
+++ components/deleteButton.tsx
@@ -1,20 +1,25 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import { Trash2 } from "lucide-react";
 import { deleteFileFromFirebase } from "@/actions/serverActions";
 import { useRouter } from "next/navigation";
 
 export const DeleteButton = ({ filePath }: { filePath: string }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   return (
     <Button
       onClick={async () => {
+        setIsLoading(true);
         await deleteFileFromFirebase(filePath);
         router.refresh();
       }}
+      className="hover:bg-red-500"
     >
-      <Trash2 />
+      <span className={isLoading ? "animate-spin " : ""}>
+        <Trash2 />
+      </span>
     </Button>
   );
 };
