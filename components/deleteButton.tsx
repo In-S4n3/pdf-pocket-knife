5cd13f020428941b509e44644a70431389bf1180
In-S4n3 <tiagodcpp@gmail.com>
2023-08-28 16:07:48 +0100

    improvements

--- components/deleteButton.tsx
+++ components/deleteButton.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import { Button } from "./ui/button";
 import { Trash2 } from "lucide-react";
 import { deleteFileFromFirebase } from "@/actions/serverActions";
+import { useRouter } from "next/navigation";
 
 export const DeleteButton = ({ filePath }: { filePath: string }) => {
+  const router = useRouter();
   return (
     <Button
       onClick={async () => {
         await deleteFileFromFirebase(filePath);
+        router.refresh();
       }}
     >
       <Trash2 />
