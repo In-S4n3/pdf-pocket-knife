2204eea32737bc8223dfdc99bb150f0881dd15b1
In-S4n3 <tiagodcpp@gmail.com>
2023-08-29 17:32:08 +0100

    improvements

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -16,14 +16,9 @@ export default function LandingLayout({
   if (isMounted === false) return;
 
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body className="min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700">
-          <Navbar />
-          {children}
-        </body>
-      </html>
-    </>
+    <main className="min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-gray-700">
+      <Navbar />
+      {children}
+    </main>
   );
 }
